2c8318e93e67a7429fc930e131790c13a71066c8
unknown <kirillgilead@gmail.com>
2022-04-30 17:41:51 +0300

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({
-  static: './build'
-})
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
 
 const PORT = process.env.PORT || 3001
 
-server.use(middlewares)
-server.use(router)
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
 
-server.listen(PORT, () => {
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
   console.log('Server is running')
 })
